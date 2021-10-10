b6f53467ad9cdba3ff1cc758de588da0306d5f62
JEREMY <19DECEmbre>
2021-10-11 00:24:55 +0200

    feat : ColumnHelper

---
+++ commons-utils/src/test/java/com/jmc/commons/utils/constants/ColumnHelperTest.java
@@ -0,0 +1,17 @@
+package com.jmc.commons.utils.constants;
+
+import com.jmc.commons.utils.AbstractFinalClassHelperTest;
+import com.jmc.commons.utils.constants.database.ColumnHelper;
+
+/**
+ * @author Jeremy MARTIN CATANI
+ * @created 10/10/2021
+ */
+public class ColumnHelperTest extends AbstractFinalClassHelperTest {
+
+	@Override
+	protected void setClassToTest() {
+		CLASS_TO_TEST = ColumnHelper.class;
+	}
+
+}
