eac5cf16a619c448341801f3b48e1e1a78711a74
JEREMY <19DECEmbre>
2021-10-11 00:26:49 +0200

    feat : PermissionType

---
+++ commons-utils/src/main/java/com/jmc/commons/utils/enums/permission/PermissionType.java
@@ -0,0 +1,13 @@
+package com.jmc.commons.utils.enums.permission;
+
+/**
+ * Enum type that indicates the permission of entities
+ *
+ * @author Jeremy MARTIN CATANI
+ * @created 07/10/2021
+ */
+public enum PermissionType {
+
+	HISTORY, PERMISSION, ROLE, USER
+
+}
