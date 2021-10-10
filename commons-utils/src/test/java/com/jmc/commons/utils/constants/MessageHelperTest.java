7495f6c98f2567726de05be3199036fbec89a763
JEREMY <19DECEmbre>
2021-10-11 00:26:22 +0200

    feat : MessageHelper

---
+++ commons-utils/src/test/java/com/jmc/commons/utils/constants/MessageHelperTest.java
@@ -0,0 +1,17 @@
+package com.jmc.commons.utils.constants;
+
+import com.jmc.commons.utils.AbstractFinalClassHelperTest;
+import com.jmc.commons.utils.constants.message.MessageHelper;
+
+/**
+ * @author Jeremy MARTIN CATANI
+ * @created 10/10/2021
+ */
+public class MessageHelperTest extends AbstractFinalClassHelperTest {
+
+	@Override
+	protected void setClassToTest() {
+		CLASS_TO_TEST = MessageHelper.class;
+	}
+
+}
