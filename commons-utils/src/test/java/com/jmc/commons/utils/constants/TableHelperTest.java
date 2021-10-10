12f91945557def15b3a77ec6630a947b2e97d9ee
JEREMY <19DECEmbre>
2021-10-11 00:28:26 +0200

    feat : TableHelper

---
+++ commons-utils/src/test/java/com/jmc/commons/utils/constants/TableHelperTest.java
@@ -0,0 +1,17 @@
+package com.jmc.commons.utils.constants;
+
+import com.jmc.commons.utils.AbstractFinalClassHelperTest;
+import com.jmc.commons.utils.constants.database.TableHelper;
+
+/**
+ * @author Jeremy MARTIN CATANI
+ * @created 10/10/2021
+ */
+public class TableHelperTest extends AbstractFinalClassHelperTest {
+
+	@Override
+	protected void setClassToTest() {
+		CLASS_TO_TEST = TableHelper.class;
+	}
+
+}
