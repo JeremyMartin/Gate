cc56762ec26211ee573f25faa455c98fdec2af84
JEREMY <19DECEmbre>
2021-10-11 00:25:19 +0200

    feat : EventType

---
+++ commons-utils/src/test/java/com/jmc/commons/utils/enums/history/EventTypeTest.java
@@ -0,0 +1,16 @@
+package com.jmc.commons.utils.enums.history;
+
+import com.jmc.commons.utils.AbstractEnumTest;
+
+/**
+ * @author Jeremy MARTIN CATANI
+ * @created 10/10/2021
+ */
+public class EventTypeTest extends AbstractEnumTest {
+
+	@Override
+	protected void setClassToTest() {
+		CLASS_TO_TEST = EventType.class;
+	}
+
+}
