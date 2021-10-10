dafd8f9d961083cc77afd3ad30889e791815ee3a
JEREMY <19DECEmbre>
2021-10-11 00:28:36 +0200

    feat : TokenType

---
+++ commons-utils/src/main/java/com/jmc/commons/utils/enums/token/TokenType.java
@@ -0,0 +1,13 @@
+package com.jmc.commons.utils.enums.token;
+
+/**
+ * Enum type that indicates the action of token
+ *
+ * @author Jeremy MARTIN CATANI
+ * @created 07/10/2021
+ */
+public enum TokenType {
+
+	EMAIL, PASSWORD
+
+}
---
+++ commons-utils/src/test/java/com/jmc/commons/utils/enums/token/TokenTypeTest.java
@@ -0,0 +1,16 @@
+package com.jmc.commons.utils.enums.token;
+
+import com.jmc.commons.utils.AbstractEnumTest;
+
+/**
+ * @author Jeremy MARTIN CATANI
+ * @created 10/10/2021
+ */
+public class TokenTypeTest extends AbstractEnumTest {
+
+	@Override
+	protected void setClassToTest() {
+		CLASS_TO_TEST = TokenType.class;
+	}
+
+}
