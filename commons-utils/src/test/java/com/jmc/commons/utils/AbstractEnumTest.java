051e72e4d4b0469c29b700cbb719545d29c2e8bd
JEREMY <19DECEmbre>
2021-10-11 00:24:25 +0200

    test : prepare abstract

--- commons-utils/src/test/java/com/jmc/commons/utils/AbstractEnumTest.java
+++ commons-utils/src/test/java/com/jmc/commons/utils/AbstractEnumTest.java
@@ -4,20 +4,15 @@ import org.junit.jupiter.api.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Modifier;
-
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.fail;
+import java.lang.reflect.Method;
 
 /**
  * @author Jeremy MARTIN CATANI
- * @created 08/10/2021
+ * @created 10/10/2021
  */
 @TestInstance(value = TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(value = MethodOrderer.OrderAnnotation.class)
-public abstract class AbstractFinalClassHelperTest {
+public abstract class AbstractEnumTest {
 
 	protected static long START_GLOBAL_TIME;
 	protected static long START_EACH_TIME;
@@ -50,17 +45,16 @@ public abstract class AbstractFinalClassHelperTest {
 
 	@Test
 	@Order(value = 0)
-	public void privateConstructorTest() {
+	public void valueOfEnumTest() {
 		this.setClassToTest();
 		try {
-			Constructor<?> constructor = CLASS_TO_TEST.getDeclaredConstructor();
-			if (!Modifier.isPrivate(constructor.getModifiers())) {
-				fail("Constructor is not private");
+			Method method = CLASS_TO_TEST.getMethod("values");
+			Object[] objects = (Object[]) method.invoke(null);
+			for (Object o : objects) {
+				CLASS_TO_TEST.getMethod("valueOf", String.class).invoke(null, o.toString());
 			}
-			constructor.setAccessible(Boolean.TRUE);
-			constructor.newInstance();
-		} catch (NoSuchMethodException | InvocationTargetException | InstantiationException | IllegalAccessException ex) {
-			assertNull(ex.getMessage(), "Exception message must be null");
+		} catch (Exception e) {
+			LOG.error(e.getMessage(), e);
 		}
 	}
 
